b13267aa6defbb325f6e661632063d3d6d5a8237
seoyeonko <seoyeon99o@naver.com>
2023-06-27 21:22:31 +0900

    fix: use entity code

--- app/head.tsx
+++ app/head.tsx
@@ -1,7 +1,7 @@
 export default function Head() {
   return (
     <>
-      <title>Sean&#146;s Blog</title>
+      <title>Sean&rsquo;s Blog</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <link rel="icon" href="/favicon.ico" />
     </>
